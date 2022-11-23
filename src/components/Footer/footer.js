0034acaa89d87e5d99656e1789939aa3c375ed53
Jay Dry <30884232+Jaydendry@users.noreply.github.com>
2022-11-24 01:28:19 +0800

    foot

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -6,7 +6,7 @@ const Footer = (props) => {
    <div className="foot-container"  style={{backgroundColor: `${props.colorFlat}`}}>
     <div className="flexFoot">
       <div className="textArea">
-        <h1>Have an idea?</h1><h3>Send Me a Message</h3>
+        <h1>Have an idea?</h1><h3>Send Me a Message</h3><a href="mailto:jay@jaydendry.com"><h1>jay@jaydendry.com</h1></a>
       </div>
         <SocialsMinimal />
     </div>
