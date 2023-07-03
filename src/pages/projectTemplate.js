56a1dc7288500a84b8d837a972c8de5094aa0bd1
Jay Dry <30884232+Jaydendry@users.noreply.github.com>
2023-07-03 15:31:18 +0800

    yes

--- src/pages/projectTemplate.js
+++ src/pages/projectTemplate.js
@@ -1,4 +1,5 @@
-import React, {useEffect} from 'react'
+
+import { useEffect } from 'react';
 import Text from '../components/textBox/text'
 import NextProject from '../components/nextProject/nextProject'
 import Hero from '../components/Hero/hero'
@@ -6,84 +7,76 @@ import ImgContainer from '../components/imgContainer/imgContainer'
 import Persona from '../components/Persona/persona'
 import Footer from '../components/Footer/footer'
 import Navigation from '../components/Navigation/navigation'
-import { motion, useIsPresent } from "framer-motion";
 
+export default function ProjectPage(props) {
+  const { heading, roles, color, colorFlat, hero, abrProject, image1, image2, image3, image4, processA, processB, processC, conclusionA, conclusionB, conclusionC, image5, image6, image7, image8, image9, image10, nextName, nextRole, nextImage, nextPage } = props;
 
-export default function ProjectPage (props) {
-  const isPresent = useIsPresent();
   useEffect(() => {
-    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
   }, []);
-  return (
-    <div>
-      <Navigation />
-      <Hero 
-        heading={props.heading} 
-        headingLineTwo={props.roles} 
-        bgImg={props.hero} 
-        color={props.color} 
-        colorFlat={props.colorFlat}
-      />
-      <ImgContainer 
-        abrProject={props.abrProject} 
-        image1={props.image1}
-      />
-      <Text 
-        colorFlat={props.colorFlat} 
-        heading="PROJECT OVERVIEW" 
-        contentA={props.overviewA} 
-        contentB={props.overviewB} 
-        contentC={props.overviewC}
-      />
-      <ImgContainer 
-        abrProject={props.abrProject} 
-        image1={props.image2} 
-        image2={props.image3} 
-        image3={props.image4}
-      />
-      <Text 
-        colorFlat={props.colorFlat}
-        heading="PROCESS" 
-        contentA={props.processA} 
-        contentB={props.processB} 
-        contentC={props.processC}
-      />
-      <ImgContainer 
-        abrProject={props.abrProject} 
-        image1={props.image5} 
-        image2={props.image6} 
-        image3={props.image7}
-      />
-      <Text 
-        colorFlat={props.colorFlat} 
-        heading="CONCLUSION" 
-        contentA={props.conclusionA} 
-        contentB={props.conclusionB} 
-        contentC={props.conclusionC}
-      />
-      <ImgContainer 
-        abrProject={props.abrProject} 
-        image1={props.image8} 
-        image2={props.image9} 
-        image3={props.image10}
-      />
-      <Persona />
-      <Persona />
-      <Persona />
-      <NextProject 
-        name={props.nextName} 
-        role={props.nextRole} 
-        image={props.nextImage} 
-        link={props.nextPage} 
-      />
-      <Footer />
-      {/* <motion.div
-        initial={{ scaleX: 1 }}
-        animate={{ scaleX: 0, transition: { duration: 1, ease: "circOut" } }}
-        exit={{ scaleX: 2, transition: { duration: 1, ease: "circIn" } }}
-        style={{ originX: isPresent ? 1 : 0 }}
-        className="privacy-screen"
-      /> */}
-    </div> 
-  )
+return (
+  <div>
+    <Navigation />
+    <Hero
+      heading={heading}
+      headingLineTwo={roles}
+      bgImg={hero}
+      color={color}
+      colorFlat={colorFlat}
+    />
+    <ImgContainer
+      abrProject={abrProject}
+      image1={image1}
+    />
+    <Text
+      colorFlat={colorFlat}
+      heading="PROJECT OVERVIEW"
+      contentA={props.overviewA}
+      contentB={props.overviewB}
+      contentC={props.overviewC}
+    />
+    <ImgContainer
+      abrProject={abrProject}
+      image1={image2}
+      image2={image3}
+      image3={image4}
+    />
+    <Text
+      colorFlat={colorFlat}
+      heading="PROCESS"
+      contentA={processA}
+      contentB={processB}
+      contentC={processC}
+    />
+    <ImgContainer
+      abrProject={abrProject}
+      image1={image5}
+      image2={image6}
+      image3={image7}
+    />
+    <Text
+      colorFlat={colorFlat}
+      heading="CONCLUSION"
+      contentA={conclusionA}
+      contentB={conclusionB}
+      contentC={conclusionC}
+    />
+    <ImgContainer
+      abrProject={abrProject}
+      image1={image8}
+      image2={image9}
+      image3={image10}
+    />
+    <Persona />
+    <Persona />
+    <Persona />
+    <NextProject
+      name={nextName}
+      role={nextRole}
+      image={nextImage}
+      link={nextPage}
+    />
+    <Footer />
+  </div>
+);
 }
