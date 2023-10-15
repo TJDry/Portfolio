f17a20fc60ecb39caa8618f4359ea82ed8466d17
Jay Dry <30884232+Jaydendry@users.noreply.github.com>
2023-10-15 08:23:41 +0800

    KERMIT THE MOBILE

--- src/components/Navigation/navigation.jsx
+++ src/components/Navigation/navigation.jsx
@@ -1,4 +1,4 @@
-import React, {useState,useRef} from 'react'
+import React, {useState,useRef,useEffect} from 'react'
 import { NavLink } from "react-router-dom";
 import styles from './navigation.module.scss'
 import { navigationData } from '../Navigation/navigationData';
@@ -9,6 +9,23 @@ export default function Navigation() {
   let activeClass = "active"
   const {scrollY} = useScroll();
   const [hidden, setHidden] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const [isOpen,setIsOpen] = useState(false);
+  const openRef = useRef();
+
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth <= 650);
+    }
+
+    handleResize(); // Set initial isMobile value on component mount.
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   function update() {
     if (scrollY?.current < scrollY?.prev) {
@@ -26,6 +43,8 @@ export default function Navigation() {
     visible: { opacity: 1, y: 0 },
     hidden: { opacity: 0, y: -5 }
   };
+
+
   return (
     <motion.div className={styles.container}
       variants={variants}
@@ -33,7 +52,49 @@ export default function Navigation() {
       transition={{ ease: [0.2, 0.3, 0.4, 1], duration: 0.3 }}
     >
       <h3><Logo/></h3>
-      <ul className={styles.navigation}>
+      {isMobile ? (
+      <div className={styles.hamburgerMenu}>
+          <div className={styles.navigationContainer} onClick={() => setIsOpen(!isOpen)}>
+              <div className={isOpen ? `${styles.navBtn} ${styles.active}` : `${styles.navBtn}`}></div>
+          </div>
+              <div 
+                  className={styles.navMenu} 
+                  ref={openRef}
+                  style={isOpen 
+                  ? {
+                      width: "100vw",
+                  }:{
+                      width: "0px",
+                  }}
+                  >
+              <ul>
+                  {navigationData.map((item, index) => {
+                      return(
+                          <>
+                          <li 
+                          key={index} 
+                          className={`${styles.navText} ${styles[item.cName]}`}
+                          style={isOpen 
+                          ? {
+                              width: "40%",
+                              opacity: "1",
+                          }:{
+                              width: "0",
+                              opacity: "0"
+                          }}
+                          >
+                              <NavLink to={`/${item.path}`}>
+                                  <span>{item.title}</span>
+                              </NavLink>
+                          </li>
+                          </>
+                      );
+                  })}
+              </ul>
+          </div>
+      </div>
+      ):(
+        <ul className={styles.navigation}>
         {navigationData.map(item => {
           return(
               <li key={item.id}>
@@ -48,6 +109,7 @@ export default function Navigation() {
             );
           })}
       </ul>
+      )}
     </motion.div>
   )
 }
