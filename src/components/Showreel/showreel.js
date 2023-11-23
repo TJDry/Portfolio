44b10ef53082e93f09fbecc542112234a92e2393
Jay Dry <30884232+Jaydendry@users.noreply.github.com>
2023-11-23 18:40:19 +0800

    Showreelanimation

--- src/components/Showreel/showreel.js
+++ src/components/Showreel/showreel.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './showreelStyle.module.scss'
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 const Showreel = () =>{
-
+    const showreelScale = useRef(null);
+    useEffect(()=>{
+      const element = showreelScale.current;
+      gsap.fromTo(
+        element.children,
+        {
+          scale: 0.8,
+        },
+        {
+          scale: 1,
+          duration: 5,
+          ease: "power1",
+          scrollTrigger: {
+            trigger: element,
+            scrub: true,
+            start: "-100%",
+            end: "top center",
+            markers: true
+          }
+        }
+      );
+    }, []);
     return(
-        <div className={styles.vidContainer}>
+        <div ref={showreelScale} className={styles.vidContainer}>
             <video width="80%" loop autoPlay muted>
                 <source src={process.env.PUBLIC_URL + "/videos/DigEx_Jayden_Dry_Showreel.mp4"} type="video/mp4"/>
             </video>
